30d6961764e8adcfaf6b8dd28db6ca049adefab7
Kaiyuan Wang <me@iamkaiyuan.wang>
2019-09-08 18:32:36 +0800

    front end updated
    1. fixed chart updating issue
    2. minor change in logger method

--- logger.py
+++ logger.py
@@ -1,7 +1,7 @@
 import logging
 
 
-def init_logger(logger_name, filename='mylogs.log'):
+def init_logger(logger_name, level='WARNING', filename='mylogs.log'):
     """The getLogger helper
     Usage example:
 
@@ -21,9 +21,24 @@ def init_logger(logger_name, filename='mylogs.log'):
     :param filename: string: logger filename
     :return: Logger class
     """
+    _level_dict = {
+        # A serious error, indicating that the program itself may be unable to continue running.
+        'CRITICAL': logging.CRITICAL,
+        # Due to a more serious problem, the software has not been able to perform some function.
+        'ERROR': logging.ERROR,
+        # An indication that something unexpected happened, or indicative of some problem in the near
+        # future (e.g. ‘disk space low’). The software is still working as expected.
+        'WARNING': logging.WARNING,
+        # Confirmation that things are working as expected.
+        # Report events that occur during normal operation of a program
+        # (e.g. for status monitoring or fault investigation)
+        'INFO': logging.INFO,
+        # Detailed information, typically of interest only when diagnosing problems.
+        'DEBUG': logging.DEBUG
+    }
     log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logger = logging.getLogger(logger_name)
-    logger.setLevel(logging.DEBUG)
+    logger.setLevel(_level_dict.get(level.upper()))
     log_file_handler = logging.FileHandler(filename)
     log_formatter = logging.Formatter(log_format)
     log_file_handler.setFormatter(log_formatter)
