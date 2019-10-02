2900d0e296665f3df63a7cb4ce06ca79093aefcc
Kaiyuan Wang <me@iamkaiyuan.wang>
2019-10-02 23:22:16 +0800

    write sys.log to sd card if the card is mounted, otherwise to internal flash

--- logger.py
+++ logger.py
@@ -22,9 +22,17 @@ def init_logger(logger_name, filename='sys.log'):
     :return: Logger class
     """
     import logging
+
+    if 'sd' in uos.listdir():
+        if not 'log' in uos.listdir('sd'):
+            uos.mkdir('sd/log')
+        filepath = '/sd/log/' + filename
+    else:
+        filepath = filename
+
     log_format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
     logger = logging.getLogger(logger_name)
-    log_file_handler = logging.FileHandler(filename)
+    log_file_handler = logging.FileHandler(filepath)
     log_formatter = logging.Formatter(log_format)
     log_file_handler.setFormatter(log_formatter)
     logger.addHandler(log_file_handler)
