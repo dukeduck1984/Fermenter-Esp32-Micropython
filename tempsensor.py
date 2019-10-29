2a7e2a89e2dcadda40bc3dfa7f36a0c25a9ca64c
Kaiyuan Wang <me@iamkaiyuan.wang>
2019-10-29 18:38:28 +0800

    ds18b20 sensor now is assigned by it's romcode instead of device number

--- tempsensor.py
+++ tempsensor.py
@@ -3,38 +3,33 @@ import onewire
 import ds18x20
 
 
-# class RomCodeConvert:
-#     @staticmethod
-#     def _to_romcode_string(bytearray_romcode):
-#         """
-#         把bytearray格式的rom code转换为字符串格式
-#         如：bytearray_romcode = bytearray(b'(\xaa4\xe4\x18\x13\x02;')
-#         转换后为：'28 aa 34 e4 18 13 02 3b'
-#         :param bytearray_romcode: bytearray
-#         :return: str
-#         """
-#         import ubinascii
-#         string_hex_list = [str(ubinascii.hexlify(bytes([el])), 'utf8') for el in bytearray_romcode]
-#         romcode_str = ' '.join(string_hex_list)
-#         return romcode_str
-#
-#     @staticmethod
-#     def _to_romcode_bytearray(string_romcode):
-#         """
-#         把字符串格式的rom code转换为bytearray格式
-#         如：string_romcode = '28 aa 34 e4 18 13 02 3b'
-#         转换后为：bytearray(b'(\xaa4\xe4\x18\x13\x02;')
-#         :param string_romcode: str
-#         :return: bytearray
-#         """
-#         import ubinascii
-#         string_hex_list = string_romcode.split()
-#         bytes_list = [int.from_bytes(bytes(ubinascii.unhexlify(el)), 'big') for el in string_hex_list]
-#         romcode_bytearray = bytearray(bytes_list)
-#         return romcode_bytearray
+class RomCodeConvert:
+    @staticmethod
+    def from_romcode_to_hex_string(romcode_bytearray):
+        """
+        把bytearray格式的rom code转换为字符串格式
+        如：romcode_bytearray = bytearray(b'(\xaa\xec\x01\x19\x13\x028')
+        转换后为：'0x28aaec0119130238'
+        :param romcode_bytearray: bytearray
+        :return: str
+        """
+        romcode_int = int.from_bytes(romcode_bytearray, 'big')
+        return hex(romcode_int)
+
+    @staticmethod
+    def from_hex_string_to_romcode(hex_string):
+        """
+        把字符串格式的rom code转换为bytearray格式
+        如：hex_string = '0x28aaec0119130238'
+        转换后为：bytearray(b'(\xaa\xec\x01\x19\x13\x028')
+        :param hex_string: str
+        :return: bytearray
+        """
+        romcode_int = int(hex_string, 0)
+        return bytearray(romcode_int.to_bytes(8, 'big'))
 
 
-class Ds18Sensors:
+class Ds18Sensors(RomCodeConvert):
     def __init__(self, pin):
         """
         Initialize the DS18 temperature sensor
@@ -48,8 +43,9 @@ class Ds18Sensors:
     def get_device_list(self):
         self.device_list = self.ds.scan()
         return [
-            {'value': device_number, 'label': hex(int.from_bytes(bytearray_romcode, ''))}
-            for device_number, bytearray_romcode in enumerate(self.device_list)
+            {'value': self.from_romcode_to_hex_string(bytearray_romcode),
+             'label': self.from_romcode_to_hex_string(bytearray_romcode)}
+            for bytearray_romcode in self.device_list
         ]
 
     def get_device_qty(self):
@@ -70,30 +66,39 @@ class Ds18Sensors:
             self.last_reading_available = True
 
 
-class SingleTempSensor:
-    def __init__(self, ds_obj, device_number):
+class SingleTempSensor(RomCodeConvert):
+    def __init__(self, ds_obj, romcode_hex_string):
         self.ds_obj = ds_obj
-        self.update_device_num(device_number)
+        self.is_connected = False
+        self.bytearray_romcode = self.update_romcode(romcode_hex_string)
 
     def read_temp(self):
         if not self.ds_obj.last_reading_available:
             self.ds_obj.get_realtime_temp()
-        if self.bytearray_romcode:
-            try:
-                temp = round(self.ds_obj.ds.read_temp(self.bytearray_romcode), 1)
-            except Exception as e:
-                print('The DS18 sensor was disconnected.  Check the wire.')
-                return -99.9
-            else:
-                return temp
-        else:
+        try:
+            temp = round(self.ds_obj.ds.read_temp(self.bytearray_romcode), 1)
+        except Exception as e:
+            self.is_connected = False
+            self.ds_obj.device_list = None
+            print('The DS18 sensor was disconnected.  Check the wire.')
             return -99.9
-
-    def update_device_num(self, new_device_number):
-        if new_device_number + 1 <= self.ds_obj.get_device_qty():
-            self.bytearray_romcode = self.ds_obj.device_list[new_device_number]
         else:
-            self.bytearray_romcode = None
-            print('Invalid Device Number!')
+            self.is_connected = True
+            return temp
 
+    def update_romcode(self, new_romcode_hex_string):
         self.ds_obj.last_reading_available = False
+        try:
+            new_romcode_bytearray = self.from_hex_string_to_romcode(new_romcode_hex_string)
+        except Exception as e:
+            print(e)
+            return None
+        if new_romcode_bytearray in self.ds_obj.ds.scan():
+            self.is_connected = True
+        else:
+            self.is_connected = False
+            print('DS18B20 ' + new_romcode_hex_string + ' not connected.')
+        return new_romcode_bytearray
+
+    def is_connected(self):
+        return self.is_connected
